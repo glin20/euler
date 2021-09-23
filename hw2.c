321c857f1c357f4ad661ddaad6905b3fc0a93e80
GLin9104 <geoffreypokemon@gmail.com>
2021-09-22 20:58:08 -0400

    stuff

--- hw2.c
+++ hw2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int question1(int n){
+int question1(){
   int i;
   int total = 0;
-  for (i = 0; i < n; i++){
+  for (i = 0; i < 1000; i++){
     if (i % 3 == 0 || i % 5 == 0){
       total += i;
     }
@@ -12,27 +12,26 @@ int question1(int n){
 int question5(){
   int x = 2520;
   while (x %  2 != 0 || x %  3 != 0 || x %  4 != 0 || x %  5 != 0 ||
-    x %  6 != 0 || x %  7 != 0 || x %  8 != 0 || x %  9 != 0 ||
-    x % 10 != 0 || x % 11 != 0 || x % 12 != 0 || x % 13 != 0 ||
-    x % 14 != 0 || x % 15 != 0 || x % 16 != 0 || x % 17 != 0 ||
-    x % 18 != 0 || x % 19 != 0 || x % 20 != 0 ){
-      x++;
+         x %  6 != 0 || x %  7 != 0 || x %  8 != 0 || x %  9 != 0 ||
+         x % 10 != 0 || x % 11 != 0 || x % 12 != 0 || x % 13 != 0 ||
+         x % 14 != 0 || x % 15 != 0 || x % 16 != 0 || x % 17 != 0 ||
+         x % 18 != 0 || x % 19 != 0 || x % 20 != 0 ){
+    x++;
   }
   return x;
 }
-int question6(int n){
+int question6(){
   int i;
-  int sumofsquare;
-  int squareofsum;
-  sumofsquare = n*(n + 1)*(2*n + 1)/6;
-  squareofsum = n*(n + 1)/2;
+  int sumofsquare = 0;
+  int squareofsum = 0;
+  for (i = 1; i <= 100; i++){
+    sumofsquare += i * i;
+    squareofsum += i;
+  }
   return squareofsum * squareofsum - sumofsquare;
 }
 int main(){
-  int q1 = question1(1000);
-  int q5 = question5();
-  int q6 = question6(100);
-  printf("%d\n", q1);
-  printf("%d\n", q5);
-  printf("%d\n", q6);
+  printf("%d\n", question1());
+  printf("%d\n", question5());
+  printf("%d\n", question6());
 }
